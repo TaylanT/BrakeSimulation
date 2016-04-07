7eb355148709ad96d7fe7dfc1db6fc6229d1cb65
Taylan Tokan <tokan@ift.uni-hannover.de>
2016-04-07 16:15:59 +0200

    Erste Ergebnisse
    Energiebilanz über W=Fs und Q=mcdT

--- brakeFriction.py
+++ brakeFriction.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import math
 
-pi=math.pi()
+pi=math.pi
 
 #erste Ideen für Temperaturverteilung in Bremse
 
@@ -27,7 +30,7 @@ rInnen=0.1
 #Klotz Reibfläche
 radKlotz=0.1
 tanKlotz=0.1
-AKlotz=bKlotz*lKlotz
+AKlotz=radKlotz*tanKlotz
 
 #Stoffwerte [J/kgK]
 cScheibe=500
@@ -40,10 +43,13 @@ nu=0.3
 def masse(rInnen,radKlotz,bBrake):
 	#dichte 7.2 [g/cm3]
 	dichteBrake=1000*7.2
+	#Fläche Kreisring
 	ARing=pi*((rInnen+radKlotz)**2-rInnen**2)
+	#Volumen Kreisring
 	VolRing=ARing*bBrake
-	m=VolRing*dichteBrake
-	return m
+	mRing=VolRing*dichteBrake
+	return mRing
+	
 
 
 
